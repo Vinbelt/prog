0e283a0a44b21389a954ab7ee62f5a12409a15b6
Beltran Viñado Domeque <125213021+Vinbelt@users.noreply.github.com>
2026-02-17 08:29:58 +0000

    ordenacion minimo

---
+++ ordenarMin.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+void printarr(int n, int a[]);
+
+int main(void){
+	int N;
+	cout << "Numeros: ";
+	cin >> N;
+	cout << endl;
+
+	int arr[N];
+	
+	for (int i=0; i<N; i++){
+		cout << "N" << i+1 << " >> ";
+		cin >> arr[i];
+	}
+	
+	printarr(N, arr);
+	
+	//Algorithm runtime O(n²)
+	int min, posmenor, temp;
+	for (int i=0; i<N; i++){
+		min = arr[i], posmenor = i;
+		
+		//Check for minor
+		for (int j=0; j<N; j++){
+			if (min > arr[j]){
+				min = arr[j], posmenor = j;
+			}
+		}
+		
+		//Swap
+		temp = arr[i];
+		arr[i] = arr[posmenor];
+		arr[posmenor] = temp;
+		
+	}
+
+	printarr(N, arr);
+}
+
+
+void printarr(int n, int a[]){
+	for (int i=0; i<n; i++){
+		cout << a[i] << " "; 
+	}
+	cout << endl;
+}
